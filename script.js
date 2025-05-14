f0ddde9a6b4773931a064cb3667fbcceebadaa55
Vishal Kumar <98923664+ritsvishalcs@users.noreply.github.com>
2025-05-14 12:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-let totalCost = 0
-	 arr = arr.sort((a,b)=>a-b)
-		while (arr.length>1) {
-			let first = arr.shift()
-			let second = arr.shift()
-			let sum = first + second
-				totalCost+ = sum
-				arr.push(const)
-			arr.sort((a,b)=>a-b)
-		}
-  return totalCost
+function mincost(arr) { 
+  // Return the minimum cost to connect ropes
+  let totalCost = 0;
+  arr = arr.sort((a, b) => a - b);
+
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift();
+    let sum = first + second;
+
+    totalCost += sum;
+    arr.push(sum);
+    arr.sort((a, b) => a - b); // maintain sorted order
+  }
+
+  return totalCost;
 }
+
 
 module.exports=mincost;
